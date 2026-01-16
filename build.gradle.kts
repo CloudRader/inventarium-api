cc9bbc58aa72f726ae12614c6fa8067cd2d93c00
Artem Kuznetsov <artyom.20century@gmail.com>
2026-01-16 19:08:19 +0100

    chore: downgrade java and gradle version (compatability issues)

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ description = "Inventarium API for inventarisation "
 
 java {
 	toolchain {
-		languageVersion = JavaLanguageVersion.of(24)
+		languageVersion = JavaLanguageVersion.of(21)
 	}
 }
 
