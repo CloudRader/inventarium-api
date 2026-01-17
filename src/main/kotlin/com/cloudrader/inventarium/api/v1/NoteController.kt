da9063564d3d02884a8dce4cbca22e2745abf7e7
Artem Kuznetsov <artyom.20century@gmail.com>
2026-01-17 21:17:46 +0100

    chore: example note object and some other updates in structure

---
+++ src/main/kotlin/com/cloudrader/inventarium/api/v1/NoteController.kt
@@ -0,0 +1,72 @@
+package com.cloudrader.inventarium.api.v1
+
+import com.cloudrader.inventarium.model.Note
+import com.cloudrader.inventarium.repository.NoteRepository
+import org.bson.types.ObjectId
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+import java.time.Instant
+
+@RestController
+@RequestMapping("/v1/notes")
+class NoteController(
+    private val repository: NoteRepository
+) {
+
+    data class NoteRequest(
+        val id: String?,
+        val title: String,
+        val content: String,
+        val color: Long,
+        val ownerId: String?
+    )
+
+    data class NoteResponse(
+        val id: String,
+        val title: String,
+        val content: String,
+        val color: Long,
+        val createdAt: Instant,
+    )
+
+    @PostMapping
+    fun create(
+        @RequestBody body: NoteRequest
+    ): NoteResponse {
+        val note = repository.save(
+            Note(
+                id = body.id?.let { ObjectId(it) } ?: ObjectId.get(),
+                title = body.title,
+                content = body.content,
+                color = body.color,
+                createdAt = Instant.now(),
+                ownerId = ObjectId()
+            )
+        )
+
+        return note.toResponse()
+    }
+
+    @GetMapping
+    fun findByOwnerId(
+        @RequestParam(required = true) ownerId: String
+    ): List<NoteResponse> {
+        return repository.findByOwnerId(ObjectId(ownerId)).map {
+            it.toResponse()
+        }
+    }
+
+    private fun Note.toResponse() : NoteController.NoteResponse {
+        return NoteResponse(
+            id = id.toHexString(),
+            title = title,
+            content = content,
+            color = color,
+            createdAt = createdAt,
+        )
+    }
+}
---
+++ src/main/kotlin/com/cloudrader/inventarium/security/SecurityConfig.kt
@@ -0,0 +1,24 @@
+package com.cloudrader.inventarium.security
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.web.SecurityFilterChain
+
+@Configuration
+class SecurityConfig {
+
+    @Bean
+    fun filterChain(httpSecurity: HttpSecurity): SecurityFilterChain {
+        return httpSecurity
+            .csrf { csrf -> csrf.disable() }
+            .sessionManagement { it.sessionCreationPolicy(SessionCreationPolicy.STATELESS) }
+//            .authorizeHttpRequests {
+//                it
+//                    .requestMatchers("/auth/**").permitAll()
+//                    .anyRequest().authenticated()
+//            }
+            .build()
+    }
+}
