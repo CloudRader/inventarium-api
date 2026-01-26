c5693e3cc86d4e26816561b8d8dabb5a5cb7150d
Artem Kuznetsov <artyom.20century@gmail.com>
2026-01-26 22:22:26 +0100

    refactor: remove unused test files

--- src/main/kotlin/com/cloudrader/inventarium/repository/NoteRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.cloudrader.inventarium.repository
-
-import com.cloudrader.inventarium.model.Note
-import org.bson.types.ObjectId
-import org.springframework.data.mongodb.repository.MongoRepository
-
-interface NoteRepository: MongoRepository<Note, ObjectId> {
-    fun findByOwnerId(ownerId: ObjectId): List<Note>
-}
