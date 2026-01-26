5331794bd95fd0ad8ff89f2b0531803ced8d6aa1
Artem Kuznetsov <artyom.20century@gmail.com>
2026-01-26 22:24:46 +0100

    chore: add and move configs to one folder

---
+++ src/main/kotlin/com/cloudrader/inventarium/config/OpenApiConfig.kt
@@ -0,0 +1,41 @@
+package com.cloudrader.inventarium.config
+
+import io.swagger.v3.oas.models.OpenAPI
+import io.swagger.v3.oas.models.Components
+import io.swagger.v3.oas.models.security.*
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class OpenApiConfig {
+
+    @Bean
+    fun openAPI(): OpenAPI =
+        OpenAPI()
+            .components(
+                Components().addSecuritySchemes(
+                    "oauth2",
+                    SecurityScheme()
+                        .type(SecurityScheme.Type.OAUTH2)
+                        .flows(
+                            OAuthFlows()
+                                .authorizationCode(
+                                    OAuthFlow()
+                                        .authorizationUrl(
+                                            "http://localhost:9000/application/o/authorize/"
+                                        )
+                                        .tokenUrl(
+                                            "http://localhost:9000/application/o/token/"
+                                        )
+                                        .scopes(
+                                            Scopes()
+                                                .addString("openid", "OpenID")
+                                                .addString("profile", "Profile")
+                                                .addString("email", "Email")
+                                        )
+                                )
+                        )
+                )
+            )
+            .addSecurityItem(SecurityRequirement().addList("oauth2"))
+}
