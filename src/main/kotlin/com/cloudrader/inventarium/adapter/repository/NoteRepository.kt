fdadd0ba1f9c1f834b170b857ea806da8b636838
Artem Kuznetsov <artyom.20century@gmail.com>
2026-01-28 19:47:02 +0100

    chore: remove note domain from each layers

--- src/main/kotlin/com/cloudrader/inventarium/adapter/repository/NoteRepository.kt
+++
@@ -1,6 +0,0 @@
-package com.cloudrader.inventarium.adapter.repository
-
-import com.cloudrader.inventarium.model.Note
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface NoteRepository: JpaRepository<Note, String>
