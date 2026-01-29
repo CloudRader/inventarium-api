76ecd325ba226f80d3a745e25e0790a9494ff764
Artem Kuznetsov <artyom.20century@gmail.com>
2026-01-29 22:53:37 +0100

    chore: add exceptions

---
+++ src/main/kotlin/com/cloudrader/inventarium/config/exception/ForbiddenException.kt
@@ -0,0 +1,7 @@
+package com.cloudrader.inventarium.config.exception
+
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.ResponseStatus
+
+@ResponseStatus(code = HttpStatus.FORBIDDEN)
+class ForbiddenException : RuntimeException()
