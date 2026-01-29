958db6e3bcf6c5c2b65e584d8f4f244b90426c1b
Artem Kuznetsov <artyom.20century@gmail.com>
2026-01-29 23:29:40 +0100

    chore: add annotations for swagger documentation

---
+++ src/main/kotlin/com/cloudrader/inventarium/config/annotation/AuthenticatedAccount.kt
@@ -0,0 +1,14 @@
+package com.cloudrader.inventarium.config.annotation
+
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
+
+@Target(AnnotationTarget.FUNCTION, AnnotationTarget.CLASS)
+@Retention(AnnotationRetention.RUNTIME)
+@ApiResponses(
+    value = [
+        ApiResponse(responseCode = "401", description = "Unauthorized", content = [Content()]),
+    ]
+)
+annotation class AuthenticatedAccount
---
+++ src/main/kotlin/com/cloudrader/inventarium/config/annotation/FullAuthorizedAccount.kt
@@ -0,0 +1,15 @@
+package com.cloudrader.inventarium.config.annotation
+
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
+
+@Target(AnnotationTarget.FUNCTION, AnnotationTarget.CLASS)
+@Retention(AnnotationRetention.RUNTIME)
+@ApiResponses(
+    value = [
+        ApiResponse(responseCode = "401", description = "Unauthorized", content = [Content()]),
+        ApiResponse(responseCode = "403", description = "Forbidden", content = [Content()])
+    ]
+)
+annotation class FullAuthorizedAccount
