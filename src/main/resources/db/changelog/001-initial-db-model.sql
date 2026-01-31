cc5f6c963182241474558596e597494f483770a4
Artem Kuznetsov <artyom.20century@gmail.com>
2026-01-31 20:20:46 +0100

    chore: add migration for create table and instance platform_admins

--- src/main/resources/db/changelog/001-initial-db-model.sql
+++ src/main/resources/db/changelog/001-initial-db-model.sql
@@ -1,3 +1,12 @@
+CREATE TABLE platform_admins
+(
+    id            UUID PRIMARY KEY NOT NULL,
+
+    username      VARCHAR(32)      NOT NULL UNIQUE,
+
+    password_hash VARCHAR(255)     NOT NULL
+);
+
 CREATE TABLE users
 (
     id          VARCHAR(100) PRIMARY KEY,
