f5c3dedbfa4fdcc176a0c5b7e2eb035d7c6ae4f7
Artem Kuznetsov <artyom.20century@gmail.com>
2026-02-02 21:17:06 +0100

    chore: add migration create IdPs and tenants tables

---
+++ src/main/resources/db/changelog/003-create-idp-and-tenants-tables.sql
@@ -0,0 +1,33 @@
+CREATE TABLE tenants
+(
+    id         UUID PRIMARY KEY      DEFAULT gen_random_uuid(),
+
+    name       VARCHAR(255) NOT NULL,
+    alias      VARCHAR(50)  NOT NULL UNIQUE,
+
+    created_at TIMESTAMPTZ  NOT NULL DEFAULT now(),
+    updated_at TIMESTAMPTZ
+);
+
+CREATE TABLE identity_providers
+(
+    id                     UUID PRIMARY KEY      DEFAULT gen_random_uuid(),
+
+    tenant_id              UUID         NOT NULL REFERENCES tenants (id) ON DELETE CASCADE,
+
+    type                   VARCHAR(50)  NOT NULL,
+    name                   VARCHAR(100) NOT NULL,
+    alias                  VARCHAR(50)  NOT NULL UNIQUE,
+
+    client_id              VARCHAR(64)  NOT NULL,
+    client_secret_hashed   VARCHAR(255) NOT NULL,
+
+    issuer                 VARCHAR(255) NOT NULL,
+    authorization_endpoint VARCHAR(100) NOT NULL,
+    token_endpoint         VARCHAR(100) NOT NULL,
+    userinfo_endpoint      VARCHAR(255) NOT NULL,
+    jwks_uri               VARCHAR(255) NOT NULL,
+    scopes_supported       VARCHAR(255) NOT NULL,
+
+    enabled                BOOLEAN      NOT NULL DEFAULT true
+);
