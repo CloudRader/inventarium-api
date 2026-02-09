235a1410e9427ef49527d5a1f556c8251f76255d
Artem Kuznetsov <artyom.20century@gmail.com>
2026-02-09 20:59:09 +0100

    chore: update java, gradle and kotlin versions. Add detect linter

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,9 @@
 plugins {
-	kotlin("jvm") version "2.2.21"
-	kotlin("plugin.spring") version "2.2.21"
+	kotlin("jvm") version "2.3.0"
+	kotlin("plugin.spring") version "2.3.0"
 	id("org.springframework.boot") version "4.0.1"
 	id("io.spring.dependency-management") version "1.1.7"
+	id("dev.detekt") version("2.0.0-alpha.2")
 }
 
 group = "com.cloudrader"
@@ -11,7 +12,7 @@ description = "Inventarium API for inventarisation "
 
 java {
 	toolchain {
-		languageVersion = JavaLanguageVersion.of(21)
+		languageVersion = JavaLanguageVersion.of(24)
 	}
 }
 
